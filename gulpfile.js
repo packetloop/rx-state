db1832bbc753c200b54acb5d0a150e6a48d8a210
Nik Butenko <nik@butenko.me>
2014-12-10 14:17:28 +1100

    Fixed package name to rx-state

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 
 gulp.task('build', function () {
   return gulp.src(['src/**/*_module.js', 'src/**/*.js'])
-    .pipe(concat('rx-stream.js'))
+    .pipe(concat('rx-state.js'))
     .pipe(gulp.dest('./'));
 });
 
